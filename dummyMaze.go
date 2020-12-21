27d1611258200b15f615aacefc7afa88c5e3cee4
iComputeDaily <henry.haines@thetwelfth.com>
2020-12-21 10:51:08 -0500

    Add player move logic

--- dummyMaze.go
+++ dummyMaze.go
@@ -7,6 +7,11 @@ import "github.com/yourbasic/graph"
 type Maze interface {
 	Generate(width int, height int)
 	Stringify() string
+	
+	MoveLeft() bool
+	MoveUp() bool
+	MoveDown() bool
+	MoveRight() bool
 }
 
 // Represents a maze with no generation algoritum
@@ -20,7 +25,56 @@ type DummyMaze struct {
 	playerX int
 	playerY int
 }
+// Move the player left, returns weather or not the move was sucsessfull
+func (maze *DummyMaze) MoveLeft() bool {
+	playerCell := maze.playerX + (maze.playerY * maze.width)
+	
+	// If there is and edge between where the player is and where they want to move then move them, and return true
+	if maze.grid.Edge(playerCell, playerCell - 1) || maze.grid.Edge(playerCell - 1, playerCell) {
+		maze.playerX -= 1
+		return true
+	}
+	// Otherwise return false
+	return false
+}
+// Move the player up, returns weather or not the move was sucsessfull
+func (maze *DummyMaze) MoveUp() bool {
+	playerCell := maze.playerX + (maze.playerY * maze.width)
+	
+	// If there is and edge between where the player is and where they want to move then move them, and return true
+	if maze.grid.Edge(playerCell, playerCell - maze.width) || maze.grid.Edge(playerCell - maze.width, playerCell) {
+		maze.playerY -= 1
+		return true
+	}
+	// Otherwise return false
+	return false
+}
+// Move the player down, returns weather or not the move was sucsessfull
+func (maze *DummyMaze) MoveDown() bool {
+	playerCell := maze.playerX + (maze.playerY * maze.width)
+	
+	// If there is and edge between where the player is and where they want to move then move them, and return true
+	if maze.grid.Edge(playerCell, playerCell + maze.width) || maze.grid.Edge(playerCell + maze.width, playerCell) {
+		maze.playerY += 1
+		return true
+	}
+	// Otherwise return false
+	return false
+}
+// Move the player right, returns weather or not the move was sucsessfull
+func (maze *DummyMaze) MoveRight() bool {
+	playerCell := maze.playerX + (maze.playerY * maze.width)
+	
+	// If there is and edge between where the player is and where they want to move then move them, and return true
+	if maze.grid.Edge(playerCell, playerCell + 1) || maze.grid.Edge(playerCell + 1, playerCell) {
+		maze.playerX += 1
+		return true
+	}
+	// Otherwise return false
+	return false
+}
 
+// Prints the current cell
 func (maze *DummyMaze) printCurrentCell(stringyMaze *strings.Builder, v int, y int) {
 	var horizontal bool
 	var vertical bool
